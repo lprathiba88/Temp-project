4352bb06676de1f2044343f4ee31b05115b0bd5a
Jean-francois Nepton <sqasolution@hotmail.com>
2016-04-30 17:44:52 -0700

    Add Support for Logging with Log4j

--- src/test/java/com/sqa/jf/BasicAutoTest.java
+++ src/test/java/com/sqa/jf/BasicAutoTest.java
@@ -1,18 +1,15 @@
 /**
- *   File Name: BasicAutoTest.java<br>
- *
- *   Nepton, Jean-francois<br>
- *   Java Boot Camp Exercise<br>
- *   Instructor: Jean-francois Nepton<br>
- *   Created: Apr 20, 2016
- *
+ * File Name: BasicAutoTest.java<br>
+ * Nepton, Jean-francois<br>
+ * Java Boot Camp Exercise<br>
+ * Instructor: Jean-francois Nepton<br>
+ * Created: Apr 20, 2016
  */
-
 package com.sqa.jf;
 
+import org.apache.log4j.*;
 import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.*;
-import org.openqa.selenium.firefox.*;
 import org.openqa.selenium.opera.*;
 import org.openqa.selenium.safari.*;
 import org.testng.annotations.*;
@@ -27,20 +24,22 @@ import org.testng.annotations.*;
  * @author Nepton, Jean-francois
  * @version 1.0.0
  * @since 1.0
- *
  */
 public class BasicAutoTest {
 
 	static WebDriver driver;
 
+	static Logger logger = Logger.getLogger(BasicAutoTest.class);
+
 	@DataProvider(name = "UserAccountInfo")
 	public static Object[][] getData() {
 		Object[][] data = { { "sqasolution2015@gmail.com", "sqaadmin2015" },
-				{ "sqasolution2015@gmail.com", "sqaadmin2015" }, { "sqasolution2015@gmail.com", "sqaadmin2015" } };
+				{ "sqasolution2015@gmail.com", "sqaadmin2015" },
+				{ "sqasolution2015@gmail.com", "sqaadmin2015" } };
 		return data;
 	}
 
-	@BeforeClass(enabled = true, groups = "chrome")
+	@BeforeClass(enabled = false, groups = "chrome")
 	public static void setupChrome() throws InterruptedException {
 		System.setProperty("webdriver.chrome.driver", "drivers/chromedriver.exe");
 		driver = new ChromeDriver();
@@ -48,14 +47,12 @@ public class BasicAutoTest {
 		Thread.sleep(1000);
 	}
 
-	@BeforeClass(enabled = false, groups = "firefox")
+	@BeforeClass(enabled = true, groups = "firefox")
 	public static void setupFirefox() throws InterruptedException {
-		driver = new FirefoxDriver();
-		driver.get("http://gmail.com");
-		Thread.sleep(1000);
+		// driver = new FirefoxDriver();
 	}
 
-	@BeforeClass(enabled = true, groups = "chrome")
+	@BeforeClass(enabled = false, groups = "chrome")
 	public static void setupOpera() throws InterruptedException {
 		System.setProperty("webdriver.opera.driver", "drivers/operadriver.exe");
 		driver = new OperaDriver();
@@ -73,7 +70,13 @@ public class BasicAutoTest {
 	@Test(dataProvider = "UserAccountInfo")
 	public void test(String username, String password) {
 		System.out.println("Basic Test: U-" + username + "/P-" + password);
-
+		logger.info("My Information..");
+		for (int i = 0; i < 1000; i++) {
+			logger.info("Info Message " + i);
+			if (i % 5 == 0) {
+				logger.fatal("Fatal Message " + i);
+			}
+			logger.debug("Message Info " + i);
+		}
 	}
-
 }
